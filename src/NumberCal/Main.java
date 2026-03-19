ac73ddeb5f5cefab4796a65e84ac7ca779e6ab82
douzhenjun <doudouzhenjun@163.com>
2026-03-19 21:42:20 +0800

    调整文件目录结构

--- src/NumberCal/Main.java
+++
@@ -1,8 +0,0 @@
-package NumberCal;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] arr = {-1, -1, -1, -1, -1, -1};
-        NumberCalculate.numCal(0, arr, 1);
-    }
-}
